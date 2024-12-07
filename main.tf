c3ee990bd8d1a58dd00771d8c458029ed69e70c8
Piotr Kuderski <piotrkuderski@gmail.com>
2024-12-07 19:04:03 +0000

    commit 2, Sat

--- main.tf
+++ main.tf
@@ -1,1 +1,23 @@
-line1
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "eu-west-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-02141377eee7defb9"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Server"
+  }
+}
